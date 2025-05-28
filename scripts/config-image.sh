00c9a1e294b5b48f5ec47622d911e1f5c3367125
maax-cha <max@nizcorp.kr>
2025-05-28 17:30:22 +0900

    feat: do not generate ssh key on cloud-init

--- scripts/config-image.sh
+++ scripts/config-image.sh
@@ -169,15 +169,22 @@ fi
 # Update the initramfs
 chroot ${chroot_dir} update-initramfs -u
 
-# create user & do not create user on cloud-init
+# create user & do not create user on cloud-init & do not generate ssh key on cloud-init
 chroot_run "adduser --gecos ",,," --disabled-password ubuntu"
 chroot_run "sh -c 'echo "ubuntu:ubuntu" | chpasswd'"
 chroot_run "usermod -aG sudo,dialout ubuntu"
 chroot_run "echo \"ubuntu ALL=(ALL) NOPASSWD: ALL\" >> /etc/sudoers"
 echo '#cloud-config
 system_info:
-  default_user: {}' >> ${chroot_dir}/etc/cloud/cloud.cfg.d/91-disable-default-user.cfg
+  default_user: {}
+  
+ssh_deletekeys: false
+ssh_genkeytypes: []' >> ${chroot_dir}/etc/cloud/cloud.cfg.d/91-disable-default-user.cfg
 chroot_run_ubuntu "mkdir temp"
+
+# pre-generate ssh key
+chroot_run mkdir -p /etc/ssh
+chroot_run ssh-keygen -A
 
 # Update packages, 네트워크 툴 설치
 chroot_run "apt-get update"
