a7746d55afb177c7da575f7f104d885b0210638e
dadayoung0 <dadayoung09@gmail.com>
2025-05-29 10:32:36 +0900

    chore: refactor to use class

--- packages/mechaship_system/mcu_service.py
+++ packages/mechaship_system/mcu_service.py
@@ -1,141 +1,130 @@
 import os
-import subprocess
 import re
-import psutil
+import subprocess
 import time
+
+import psutil
 import serial
 
 
-def __find_ip_interface() -> str:
-    try:
-        result = subprocess.run(
-            ["ip", "route"], capture_output=True, text=True, check=True
-        )
-        output = result.stdout
+class McuService:
+    def __init__(self):
+        self.last_check_time = 0
+        self.last_percentage = 100
+        self.IP_ADDR_FAIL = [0, 0, 0, 0]
 
-        match = re.search(r"^default .* dev (\S+)", output, re.MULTILINE)
-        if match:
-            return match.group(1)
-        else:
-            return ""
-    except subprocess.CalledProcessError as e:
-        print(f"Error getting default interface: {e}")
-        return ""
-
-
-def __get_ip_addr() -> list:
-    interface = __find_ip_interface()
-    if not interface:  # default interface 없음
-        return [0, 0, 0, 0]
-
-    addrs = psutil.net_if_addrs()
-    if interface not in addrs:  # default interface에 ip 부여 안됨
-        return [0, 0, 0, 0]
-
-    for addr in addrs[interface]:
-        if addr.family.name == "AF_INET":  # IPv4
-            ip_parts = list(map(int, addr.address.split(".")))
-            return ip_parts
-
-        # IP 주소 찾지 못함
-        return [0, 0, 0, 0]
-
-    return [0, 0, 0, 0]
-
-
-def __connect_serial(
-    port="/dev/ttyMCU", baudrate=115200, timeout=1, write_timeout=1
-) -> serial.Serial:
-    while True:
+    def __find_ip_interface(self) -> str:
         try:
-            ser = serial.Serial(
-                port=port,
-                baudrate=baudrate,
-                timeout=timeout,
-                write_timeout=write_timeout,
+            result = subprocess.run(
+                ["ip", "route"], capture_output=True, text=True, check=True
             )
-            return ser
-        except serial.SerialException as e:
-            time.sleep(1)
+            output = result.stdout
 
+            match = re.search(r"^default .* dev (\S+)", output, re.MULTILINE)
+            if match:
+                return match.group(1)
+            else:
+                return ""
+        except subprocess.CalledProcessError as e:
+            print(f"Error getting default interface: {e}")
+            return ""
 
-last_check_time = 0
-last_percentage = 100
-last_20_alert = False
+    def get_ip_addr(self) -> list:
+        interface = self.__find_ip_interface()
+        addrs = psutil.net_if_addrs()
+        if interface and interface in addrs:
+            for addr in addrs[interface]:
+                if addr.family.name == "AF_INET":  # IPv4
+                    ip_parts = list(map(int, addr.address.split(".")))
+                    return ip_parts
 
+        # default interface 없음 or default interface에 ip 부여 안됨
+        # IP 주소 찾지 못함
+        return self.IP_ADDR_FAIL
 
-def __parse_rx(data: str):
-    sp = data.split(",")
-
-    if sp[0] == "$BT":  # 배터리 잔량
-        global last_check_time
-        global last_percentage
-        global last_20_alert
-        current_time = time.time()
-        percentage = float(sp[2])
-
-        if (
-            current_time - last_check_time >= 300 and percentage < last_percentage
-        ):  # 5분마다 검사, 배터리 잔량이 늘어나고 있는 경우 무시
-            if percentage < 10:
-                last_check_time = current_time
-                message = (
-                    f"⚠️ [배터리 잔량 경고]\n"
-                    f"현재 배터리 잔량이 매우 낮습니다. ({percentage:.1f}%)\n"
-                    f"시스템을 안전하게 종료한 후 배터리를 충전해주세요.\n"
+    def connect_serial(
+        self, port="/dev/ttyMCU", baudrate=115200, timeout=1, write_timeout=1
+    ) -> serial.Serial:
+        while True:
+            try:
+                ser = serial.Serial(
+                    port=port,
+                    baudrate=baudrate,
+                    timeout=timeout,
+                    write_timeout=write_timeout,
                 )
+                return ser
+            except serial.SerialException as e:
+                time.sleep(1)
 
-                subprocess.run(["wall", message])
+    def parse_rx(self, data: str) -> None:
+        sp = data.split(",")
 
-        last_percentage = percentage
+        if sp[0] == "$BT":  # 배터리 잔량
+            current_time = time.time()
+            percentage = float(sp[2])
 
-    elif sp[0] == "$PO":  # 전원 종료
-        message = f"⚠️ [시스템 종료]\n5초 뒤 시스템이 종료됩니다.\n"
-        subprocess.run(["wall", message])
+            if (
+                current_time - self.last_check_time >= 300
+                and percentage < self.last_percentage
+            ):  # 5분마다 검사, 배터리 잔량이 늘어나고 있는 경우 무시
+                if percentage < 10:
+                    self.last_check_time = current_time
+                    message = (
+                        f"⚠️ [배터리 잔량 경고]\n"
+                        f"현재 배터리 잔량이 매우 낮습니다. ({percentage:.1f}%)\n"
+                        f"시스템을 안전하게 종료한 후 배터리를 충전해주세요.\n"
+                    )
 
-        time.sleep(5)
+                    subprocess.run(["wall", message])
 
-        os.system("/usr/sbin/poweroff")
+            self.last_percentage = percentage
+
+        elif sp[0] == "$PO":  # 전원 종료
+            message = f"⚠️ [시스템 종료]\n5초 뒤 시스템이 종료됩니다.\n"
+            subprocess.run(["wall", message])
+
+            time.sleep(5)
+
+            os.system("/usr/sbin/poweroff")
 
 
 def main():
-    ser = __connect_serial()
+    mcu_service = McuService()
+    ser = mcu_service.connect_serial()
     last_ip_send_time = 0
 
     try:
         while True:
             current_time = time.time()
 
-            # IP 보고
-            if current_time - last_ip_send_time >= 5:
-                try:
-                    ip_parts = __get_ip_addr()
+            try:
+                # IP 보고
+                if current_time - last_ip_send_time >= 5:
+                    ip_parts = mcu_service.get_ip_addr()
                     ip_message = f"$IP,{ip_parts[0]},{ip_parts[1]},{ip_parts[2]},{ip_parts[3]}\r\n"
                     ser.write(ip_message.encode())
                     last_ip_send_time = current_time
-                except (serial.SerialException, OSError) as e:
-                    ser.close()
-                    ser = __connect_serial()
-                    last_ip_send_time = time.time() - 4  # 재연결 후 1초 뒤 전송
 
-            # RX
-            try:
+                # RX
                 if ser.in_waiting > 0:
                     r = ser.readline().strip().decode()
-                    __parse_rx(r)
+                    mcu_service.parse_rx(r)
 
             except (serial.SerialException, OSError) as e:
                 ser.close()
-                ser = __connect_serial()
+                ser = mcu_service.connect_serial()
                 last_ip_send_time = time.time() - 4  # 재연결 후 1초 뒤 전송
 
             time.sleep(0.1)
 
     except KeyboardInterrupt:
         pass
+
     finally:
         if ser and ser.is_open:
             ser.close()
 
 
-main()
+if __name__ == "__main__":
+    main()
